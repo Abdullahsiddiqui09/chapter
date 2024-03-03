aa9126785b59f1c6d6e11cfdf9a801c425847303
Abdullahsiddiqui09 <156638979+Abdullahsiddiqui09@users.noreply.github.com>
2024-03-04 01:13:12 +0500

    Add files via upload

---
+++ chapter 3/app.js
@@ -0,0 +1,13 @@
+// variables for number
+//question 1
+var num = "I am 15 year old  "
+alert ( num)
+//question 2
+var num1 = " you have visited this site 14 time"
+alert ( num1)
+//question 3
+var num2 = " My birth year is 1990 \nDate type of my ddeclared veriable is number  "
+document.write(num2);
+//question 4
+var num3 = "John Doe ordered 5 T-shirt(s) on XYZ Clothing store"
+document.write(num3)
---
+++ chapter4/app.js
@@ -0,0 +1,12 @@
+//VARIABLE NAMES: LEGAL & ILLEGAL
+
+var text ="  "
+document.write(text)
+var text2 = " "
+document.write(text2)
+var text3 = "e"
+document.write(text3)
+var text4 = " r"
+document.write(text4)
+var text5 ="  "
+document.write(text5)
